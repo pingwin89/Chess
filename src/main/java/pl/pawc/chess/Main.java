138d4f36a1d6111604b495c8b1241c253e46620e
pawc <pswid89@gmail.com>
2015-11-18 16:12:50 +0100

    initial commit

---
+++ src/main/java/pl/pawc/chess/Main.java
@@ -0,0 +1,9 @@
+package pl.pawc.chess;
+
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("Initializing project");
+    }
+    
+}
---
+++ src/test/java/pl/pawc/chess/MainTest.java
@@ -0,0 +1,20 @@
+package pl.pawc.chess.model;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MainTest extends TestCase{
+    public MainTest(String testName){
+        super(testName);
+    }
+
+    public static Test suite(){
+        return new TestSuite(MainTest.class);
+    }
+
+    public void testTrue(){
+	assertTrue(true);
+    }
+
+}
