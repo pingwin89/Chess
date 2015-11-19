98f6cad58a1246264bb5db87cbf0c77bf0ab651f
pawc <pswid89@gmail.com>
2015-11-19 09:50:28 +0100

    Position - translate method and second constructor

--- src/main/java/pl/pawc/chess/model/Position.java
+++ src/main/java/pl/pawc/chess/model/Position.java
@@ -1,5 +1,7 @@
 package pl.pawc.chess.model;
 
+import pl.pawc.chess.model.exception.WrongInputException;
+
 public class Position{
 
     private int x;
@@ -8,6 +10,12 @@ public class Position{
     public Position(int x, int y){
     this.x=x;
     this.y=y;
+    }
+
+    public Position(String input) throws WrongInputException{
+    Position position = translate(input);
+    this.x = position.getX();
+    this.y = position.getY();
     }
 
     public void setX(int x){
@@ -24,6 +32,40 @@ public class Position{
 
     public int getY(){
     return y;
+    }
+
+    private Position translate(String input) throws WrongInputException{
+    int x;
+    int y;
+    char[] array = input.toCharArray();
+
+        switch(String.valueOf(array[0]).toLowerCase()){
+            case "a" : y = 0; break;
+            case "b" : y = 1; break;
+            case "c" : y = 2; break;
+            case "d" : y = 3; break;
+            case "e" : y = 4; break;
+            case "f" : y = 5; break;
+            case "g" : y = 6; break;
+            case "h" : y = 7; break;
+            default : throw new WrongInputException();
+        }
+
+        try{
+           Integer.parseInt(String.valueOf(array[1]));
+        }
+        catch(NumberFormatException e){
+            throw new WrongInputException();
+        }
+
+        if(Integer.parseInt(String.valueOf(array[1]))>8||Integer.parseInt(String.valueOf(array[1]))<1){
+            throw new WrongInputException();
+        }
+        else{
+            x = Integer.parseInt(String.valueOf(array[1]))-1;
+        }
+        return new Position(x, y);
+
     }
 
 }
--- src/test/java/pl/pawc/chess/model/PositionTest.java
+++ src/test/java/pl/pawc/chess/model/PositionTest.java
@@ -4,6 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import pl.pawc.chess.model.exception.WrongInputException;
+
 public class PositionTest extends TestCase{
     public PositionTest(String testName){
         super(testName);
@@ -26,6 +28,22 @@ public class PositionTest extends TestCase{
     position.setY(4);
     assertTrue(position.getX()==3);
     assertTrue(position.getY()==4);
+    }
+
+    public void testInputConstructor(){
+        try{
+            Position position1 = new Position("A1");
+            assertTrue(position1.getX()==0&&position1.getY()==0);
+            Position position2 = new Position("D4");
+            assertTrue(position2.getX()==3&&position2.getY()==3);
+            Position position3 = new Position("E5");
+            assertTrue(position3.getX()==4&&position3.getY()==4);
+            Position position4 = new Position("H8");
+            assertTrue(position4.getX()==7&&position4.getY()==7);
+        }
+        catch(WrongInputException e){
+            fail();
+        }
     }
 
 }
